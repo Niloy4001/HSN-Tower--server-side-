a2610429ad9e47b56ae3cf27014ef416b9f1bbf3
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2025-01-14 21:22:15 +0600

    api added for get all apartments data

--- index.js
+++ index.js
@@ -36,6 +36,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const apartmentsCollection = client.db("HSNTower").collection("apartmentsCollection");
+    // const movies = database
+
+
+    // get all apartment data
+    app.get("/apartments", async(req,res)=>{
+      const cursor = await apartmentsCollection.find().toArray()
+      res.send(cursor)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
