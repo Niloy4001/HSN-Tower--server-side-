204584544b6ff71095734e13cc4d3b7e188a77e3
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2025-01-15 19:34:05 +0600

    and  comparison query added on api for filter functionality

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
 const port = 4000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(
   cors({
@@ -17,10 +17,6 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_pass}@cluster1.hcojg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,7 +25,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -37,20 +33,50 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
-    const apartmentsCollection = client.db("HSNTower").collection("apartmentsCollection");
+    const apartmentsCollection = client
+      .db("HSNTower")
+      .collection("apartmentsCollection");
     // const movies = database
 
-
     // get all apartment data
-    app.get("/apartments", async(req,res)=>{
-      const cursor = await apartmentsCollection.find().toArray()
-      res.send(cursor)
-    })
+    app.get("/apartments", async (req, res) => {
+      // filter functionality
+      const minPrice = parseInt(req.query?.min) || 0;
+      const maxPrice = parseInt(req.query?.max) || Infinity;
+
+
+      // if (minPrice && maxPrice) {
+      //   const cursor =
+      // }
+
+      // pagination functionality
+      const page = parseInt(req.query?.page) || 1;
+      const limit = 6;
+      const skip = (page - 1) * limit;
+      const cursor = await apartmentsCollection
+        .find(
+          {
+            rent: { $gte: minPrice, $lte: maxPrice },
+          }
+        )
+        .skip(skip)
+        .limit(6)
+        .toArray();
+
+      const totalApartments = await apartmentsCollection.countDocuments();
+      res.json({
+        data: cursor,
+        totalPages: Math.ceil(totalApartments / limit),
+        currentPage: page,
+      });
+      
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -58,12 +84,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
